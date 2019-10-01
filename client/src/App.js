e61c7ad81d548351396e6de286022854abbbc059
stephen <ng.jiayu.stephen@gmail.com>
2019-10-01 03:52:45 -0700

    hoverable labels

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import BarChart from "./components/BarChart/BarChart.js";
-import Map from './components/Map'
+import Map from './components/Map/Map'
 
 import "./App.css";
 
